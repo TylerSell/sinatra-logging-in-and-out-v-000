f8a28b1ef1f6d40b2cc7e715bee3e1041490d489
Tyler Sell <tylerselldesign@gmail.com>
2019-02-22 00:02:39 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,8 @@
 class Helpers
   def self.current_user(session_hash)
-    @user = User.find_by(session_hash[:user_id])
+    @user = User.find(session_hash[:user_id])
   end
+
   
   def self.is_logged_in?(session_hash)
     !!session_hash[:user_id]
