a7b42ea5dab936b217e1c678ea8258436cc68a04
Tyler Sell <tylerselldesign@gmail.com>
2019-02-22 00:25:07 +0000

    Automatically backed up by Learn

--- db/migrate/20190222002431_change_balance.rb
+++ db/migrate/20190222002431_change_balance.rb
@@ -1,5 +1,5 @@
 class ChangeBalance < ActiveRecord::Migration[5.1]
   def change
-    change_column :table_name, :column_name, :new_type
+    change_column :users, :column_name, :new_type
   end
 end
